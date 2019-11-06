ad456555aec273eedf010eb8e73d6cf8035640bb
Naeuvaseh <naeuvaseh@gmail.com>
2019-11-06 12:01:51 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
-
- /* 
-
-  Create a resuable component that can be used to generate a list of pizza toppings. Don't forget to include:
-    - Topping name
-    - Price
-    - ID
-    - (Additional properties)
- 
-  TODO:
-    #1: Generate a new "order" component (within /app) that will be the container component for our pizza toppings.
-    
-    #2: Inside /app, create an "enums" and "modles" folder.
-
-    #3: Inside /models, export an interface called Pizza with name, id, and price properties.
-
-    #4: Inside /models, export an interface called Sides with name, id, and price properties. 
-
-    #5: Inside /models, export an interface called Order with pizza, sides, tax, and total   
-        properties.
-
-    #6: Inside the Order component, create methods called addPizza, removePizza, addSide, 
-        removeSide with appropriate parameters and types.    
- 
- */  
-  
+  name = 'Angular';  
 }
