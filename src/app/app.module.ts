1561289630ead33c99b3ef41c3b5b982789304c5
Naeuvaseh <naeuvaseh@gmail.com>
2019-11-06 11:48:53 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { OrderComponent } from './order/order.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, OrderComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
