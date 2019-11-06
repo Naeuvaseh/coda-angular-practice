c9c6a7cedf85459ea8f9f175044188f1fdee8eb7
Naeuvaseh <naeuvaseh@gmail.com>
2019-11-06 11:52:14 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
-  // Create a resuable component that can be used to generate a list of pizza toppings. Don't forget to include:
-  //   - Topping name
-  //   - Price
-  //   - ID
-  //   - Additional properties
+ /* 
+
+  Create a resuable component that can be used to generate a list of pizza toppings. Don't forget to include:
+    - Topping name
+    - Price
+    - ID
+    - (Additional properties)
+ 
+  TODO:
+    #1: Generate a new "order" component (within /app) that will be the container component for our pizza toppings.
+    
+    #2: Create 
+ 
+ */  
+  
 }
